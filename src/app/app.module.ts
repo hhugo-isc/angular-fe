5ce1350c8d7eec42e37ce20a6cddab47ac6602c1
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-22 17:51:17 -0600

    Fin de la seccion: 08 Life Cycle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuestraNombreComponent,
+    Pagina1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
